e4afd45ee50f642d9db1cb6e25565d4dc28f0417
Woo Yeong CHO <46465539+JunkCho@users.noreply.github.com>
2019-01-30 01:02:03 +0900

    Update 1475.py

--- 1475.py
+++ 1475.py
@@ -1,3 +1,4 @@
+import sys
 A = input()
 N = ['0','1','2','3','4','5','6','7','8','9']
 count = [0 for z in range(10)]
@@ -8,17 +9,18 @@ for i in range(len(A)):
             count[j] += 1
         else : pass
 count
-max(count)
 for k in range(10):
     if count[k] == max(count):
         count2 += 1
 count2
-if count[9] != max(count):
+if count[0] == max(count) or count[1] == max(count) or count[2] == max(count) or count[3] == max(count) or count[4] == max(count) or count[5] == max(count) or count[7] == max(count) or count[8] == max(count):
     print(max(count))
-elif (count[9] == max(count))& count2 <=1:
-    if max(count) % 2 == 0:
-        print(int(max(count)/2))
-    elif max(count) % 2 == 1:
-        print(int((max(count)+1)/2))
-elif count[9] == max(count) & count2 >= 2 :
+    sys.exit()
+
+if count[9] < max(count) and count[6] < max(count) and count2 >= 2:
     print(max(count))
+elif count[6] == max(count) or count[9] == max(count):
+    if (count[6] + count[9]) % 2 == 1:
+        print(int(((count[6] + count[9])+1)/2))
+    elif (count[6] + count[9]) % 2 == 0:
+        print(int((count[6] + count[9])/2))
