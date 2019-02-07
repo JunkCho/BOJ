d5fb678a9d23983c2cb52df5f10a91ff68592896
Woo Yeong CHO <46465539+JunkCho@users.noreply.github.com>
2019-02-08 01:43:47 +0900

    Update 2292.py

--- 2292.py
+++ 2292.py
@@ -1,10 +1,8 @@
+import math
 N = int(input())
-i = 0
-k = 1
-while True:
-    k += i * 6
-    if k < N:
-        i += 1
-    else:
-        print(i + 1)
-        break
+D = 9-12*(1-N)
+x1 = (3 + math.sqrt(D)) / 6
+if N == 1:
+    print(1)
+else:
+    print(int(x1-0.00001)+1)
